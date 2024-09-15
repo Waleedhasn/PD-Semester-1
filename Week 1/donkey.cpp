0d2a601c4307113e6fb036a7028a2a6d8f60b557
Waleedhasn <waleed2206420@gmail.com>
2024-09-15 22:54:29 +0500

    Task

---
+++ Week 1/donkey.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout <<"                        " <<endl;
+cout <<"         ^__^ " <<endl;
+cout <<"         (oo)\\_______ " <<endl;
+cout <<"         (__)\\        )\\/\\ " <<endl;
+cout <<"              ||______w| " <<endl;
+cout <<"              ||      || " <<endl;
+cout <<"              uu      uu " <<endl;
+}
