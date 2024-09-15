70931d146846bfeb6b28b96b1cddeb5dce353e5e
Waleedhasn <waleed2206420@gmail.com>
2024-09-15 23:13:31 +0500

    Add files via upload

---
+++ gun.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{ 
+cout <<"  +--^----------,--------,-----,--------^-," <<endl;
+cout <<"   | |||||||||    '---------'    |          O" <<endl;
+cout <<"   '+----------------------------^----------|" <<endl;
+cout <<"     ',--------,--------,----------------'" <<endl;
+cout <<"      / XXXXXX /'|       /'" <<endl;
+cout <<"     / XXXXXX /  |      /'" <<endl;
+cout <<"    / XXXXXX /'--------'" <<endl;
+cout <<"   / XXXXXX /" <<endl;
+cout <<"  / XXXXXX /" <<endl;
+cout <<" (_________)" <<endl;
+cout <<" '--------'" <<endl;
+} 
