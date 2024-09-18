8e74b93edfcd1a74d1f70841fdb27833118fd3cb
Waleedhasn <waleed2206420@gmail.com>
2024-09-18 23:02:22 +0500

    Add files via upload

---
+++ speed.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout <<"Enter distance:";
+int distance;
+cin >> distance;
+cout <<"Enter time:";
+int time;
+cin >> time;
+int speed;
+speed = distance/time;
+cout <<"speed is:" << speed;
+}
